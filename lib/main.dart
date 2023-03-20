d1ee06bfb061712c7fb7c3b25e3cd5778798f299
awalerichard <richardawle182@gmail.com>
2023-03-20 17:05:48 +0200

    challenge ex3

--- lib/main.dart
+++ lib/main.dart
@@ -14,31 +14,42 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Row(
-          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
-              height: 100.0,
+              height: double.infinity,
               width: 100.0,
               // margin: EdgeInsets.only(left: 30.0),
 //padding: E    dgeInsets.all(20.0),
-              color: Colors.white,
-              child: Text('Container  1'),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              color: Colors.blue,
-              child: Text('Container 2'),
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
               color: Colors.red,
-              child: Text('Container 3'),
+              //child: Text('Container  1'),
+            ),
+            /*  SizedBox(
+              height: 20,
+            ), */
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.yellow,
+                  // child: Text('Container 2'),
+                ),
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.lightGreen,
+                  // child: Text('Container 2'),
+                ),
+              ],
+            ),
+            Container(
+              width: 100.0,
+              height: double.infinity,
+              color: Colors.blue,
+              // child: Text('Container 3'),
             ),
           ],
         )),
