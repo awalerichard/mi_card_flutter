15806b80bcd779b112e4c7d04417aea729804eb9
awalerichard <richardawle182@gmail.com>
2023-03-20 16:55:18 +0200

    containers and columns

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Row(
+          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: 100.0,
+              width: 100.0,
+              // margin: EdgeInsets.only(left: 30.0),
+//padding: E    dgeInsets.all(20.0),
+              color: Colors.white,
+              child: Text('Container  1'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.blue,
+              child: Text('Container 2'),
+            ),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.red,
+              child: Text('Container 3'),
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
